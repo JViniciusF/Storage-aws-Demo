98568316e4662e833a6c85aad4801c4797737336
JViniciusF <jose.santana20@fatec.sp.gov.br>
2023-02-09 16:11:01 -0300

    Removed unused parts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Part } from "aws-sdk/clients/s3";
 import axios from "axios";
 import { ChangeEvent, useState } from "react";
 
@@ -17,45 +16,19 @@ function Upload() {
       return null;
     }
 
-    const fileSize = file.size;
-
-    const fileType = encodeURIComponent(file.type);
-
     // @ts-ignore
 
     const { data } = await axios.get(`/api/media?fileType=${fileType}`);
 
-    const { urlSigned } = data;
-
-    const chunkSize = (1024 * 1024) * 5; // 5MB 
-
-    const numParts = Math.ceil(fileSize / chunkSize);
-
-    const chunks: Blob[] = [];
-
-    const partsUploaded: Part[] = [];
-
-    for (let index = 1; index <= numParts; index++) {
-      const start = (index - 1) * chunkSize;
-      const end = index * chunkSize;
-
-      chunks.push(
-        (index < numParts) ? file.slice(start, end) : file.slice(start)
-      );
-    };
-
-    chunks.forEach(async (chunk, index) => {
-      console.log(chunks)
-    })
-
-    return ["TESTES"]
+    //Arrumar para conseguir fazer o envio dos arquivos no padrão da AWS
+    const uploadedFile = await axios.post(data);
   }
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
 
-      <div>{file && `${file.name} - ${file.type}`}</div>
+      <div>{file && `${file.name}`}</div>
 
       <button onClick={uploadToS3}>Upload</button>
     </div>
