b1fdafae3da0b1354d6dc9ad7ab3efdcd7e03a07
JViniciusF <jose.santana20@fatec.sp.gov.br>
2023-04-14 14:33:29 -0300

    feat(UploadFile) Added new page file upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "bucket-file-storage-dev-mkplace.s3.amazonaws.com",
+      "bucket-file-storage-sandbox-mkplace.s3.amazonaws.com",
+    ]
+  },
+  env: {
+    BUCKET_URL: process.env.BUCKET_URL,
+    ACCESS_KEY: process.env.ACCESS_KEY
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,97 @@
 import axios from "axios";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import styles from '../styles/Home.module.css';
 
 function Upload() {
-  const [file, setFile] = useState<File>();
+    const [file, setFile] = useState<File>();
+    const [uploadedFile, setUploadedFile] = useState<string>();
+    const inputRef = useRef(null);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
-    }
-  };
-
-  const uploadToS3 = async () => {
-
-    if (!file) {
-      return null;
+    const clickEvent = () => {
+        // @ts-ignore
+        inputRef.current.click()
     }
 
-    // @ts-ignore
+    const handleDragOver = (event: any) => {
+        event.preventDefault();
+    };
 
-    const { data } = await axios.get(`/api/media?fileType=${fileType}`);
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            setFile(event.target.files[0]);
+        }
+    };
 
-    //Arrumar para conseguir fazer o envio dos arquivos no padrão da AWS
-    const uploadedFile = await axios.post(data);
-  }
+    const handleDrop = (event: any) => {
+        event.preventDefault();
+        setFile(event.dataTransfer?.files[0])
+    };
 
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
+    // send files to the server // learn from my other video
+    const handleUpload = async () => {
+        const formData = new FormData();
 
-      <div>{file && `${file.name}`}</div>
+        if (!file) return null
 
-      <button onClick={uploadToS3}>Upload</button>
-    </div>
-  );
+        formData.append("file", file);
+        const response = await axios.post(`/api/media`, formData, { maxBodyLength: 1000000000, maxContentLength: 1000000000 })
+
+        if (response.data) {
+            setUploadedFile(response.data)
+        }
+    };
+
+    return (
+
+        <div className={styles.container}>
+            <div className={styles.containerDropzone}>
+                <div className={styles.dropzone}
+                    onDragOver={handleDragOver}
+                    onDrop={handleDrop}
+                >
+                    <p className={styles.dropzoneText}>Drop a File</p>
+                    <p className={styles.dropzoneText}> Or </p>
+                    < input
+                        className={styles.buttonUpload}
+                        type="file"
+                        onChange={handleFileChange}
+                        ref={inputRef}
+                        hidden
+                    />
+
+                    <button className={styles.dropzoneButton} onClick={clickEvent}> Select Files </button>
+                </div>
+            </div>
+            {file &&
+                <div className={styles.containerFile}>
+                    <div className={styles.uploads}>
+                        <a className={styles.titleFiles}>Files</a>
+                        <div className={styles.rowFile}>
+                            <table className={styles.customTable}>
+                                <tr></tr>
+                                <tr className={styles.rowFile}>
+                                    <td>
+                                        <li>{file.name}</li>
+                                    </td>
+                                    <td style={{ justifyContent: "end", display: "flex" }}>
+                                        {uploadedFile &&
+                                            <button className={styles.fileButton}>
+                                                <a style={{ textDecoration: "none", color: "white" }} href={uploadedFile} type="button">Open</a>
+                                            </button>
+                                        }
+                                    </td>
+
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div className={styles.action} >
+                        < button className={styles.dropzoneButton} onClick={handleUpload} > Upload </button>
+                    </div>
+                </div>
+            }
+        </div >
+    );
 }
 
 export default Upload;
